50d9cfa8c49ecce3d47418b1f97a1e65ef0c29c2
Landon Koo <landonkoo0207@gmail.com>
2018-01-31 20:37:53 +1100

    Todo list CRUD functions added

---
+++ app/controllers/todo_lists_controller.rb
@@ -0,0 +1,42 @@
+class TodoListsController < ApplicationController
+  before_action :set_todo_list, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @todo_lists = TodoList.all
+  end
+
+  def new
+    @todo_list = TodoList.new
+  end
+
+  def create
+    @todo_list = TodoList.create(todo_list_params)
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @todo_list.update(todo_list_params)
+    redirect_to root_path
+  end
+
+  def destroy
+    @todo_list.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def todo_list_params
+    params[:todo_list].permit(:title, :description)
+  end
+
+  def set_todo_list
+    @todo_list = TodoList.find(params[:id])
+  end
+end
